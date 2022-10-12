461b7d9e23427b105a54f50a02f6b3c5b4d7d707
gabytochon <gabrielatochon@gmail.com>
2022-10-12 17:25:09 +0000

    Tablas de planeta,usuarios y favoritos añadida

--- src/main.py
+++ src/main.py
@@ -8,9 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Personaje, Planeta, Usuario
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -19,26 +18,84 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
+@app.route('/users', methods=['GET'])
+def user_list():
+    #obtener información de la base de datos
+    get_usuario = Usuario.query.all()
+    print(get_usuario)
+    #convertir la data
+    lista_usuario = list(map(lambda usuario: usuario.serialize_two(), get_usuario))
+    print(lista_usuario)
+    #enviar data
+    return jsonify(lista_usuario), 200
+@app.route('/users/<int:usuario_id>', methods=['GET'])
+def users_favorites_list(usuario_id=None):
+    if usuario_id is not None:
+        get_usuario_favorito = Usuario.query.get(usuario_id)
+        if get_usuario_favorito is not None:
+            return jsonify(get_usuario_favorito.serialize())
+        return jsonify("Not found"), 404
+    else:
+        return jsonify("Not found"), 404
+@app.route('/people', methods=['GET'])
+def people_list():
+    #obtener información de la base de datos
+    get_personaje = Personaje.query.all()
+    print(get_personaje)
+    #convertir la data
+    lista_personajes = list(map(lambda personaje: personaje.serialize(), get_personaje))
+    print(lista_personajes)
+    #enviar data
+    return jsonify(lista_personajes), 200
+@app.route('/people/<int:people_id>', methods=['GET'])
+def one_people(people_id=None):
+    if people_id is not None:
+        get_persona = Personaje.query.get(people_id)
+        if get_persona is not None:
+            return jsonify(get_persona.serialize())
+        return jsonify("Not found"), 404
+    else:
+        return jsonify("Not found"), 404
+@app.route('/planets', methods=['GET'])
+def planets_list():
+    #obtener información de la base de datos
+    get_planeta = Planeta.query.all()
+    print(get_planeta)
+    #convertir la data
+    lista_planeta = list(map(lambda planeta: planeta.serialize(), get_planeta))
+    print(lista_planeta)
+    #enviar data
+    return jsonify(lista_planeta), 200
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def one_planet(planet_id=None):
+    if planet_id is not None:
+        get_planet = Planeta.query.get(planet_id)
+        if get_planet is not None:
+            return jsonify(get_planet.serialize())
+        return jsonify("Not found"), 404
+    else:
+        return jsonify("Not found"), 404
+@app.route('/people', methods=['POST'])
+def post_people():
+    body = request.json
+    nuevo_personaje = Personaje.create_people(body)
+    if not isinstance(nuevo_personaje, Personaje):
+        print(type(nuevo_personaje), isinstance(nuevo_personaje, Personaje))
+        return jsonify({
+            "message": nuevo_personaje["message"]
+        }), nuevo_personaje["status"]
+    print(nuevo_personaje)
+    return jsonify({
+        "message": "Nuevo personaje agregado"
+    }), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,19 +1,109 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
     def __repr__(self):
         return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+class Personaje(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column (db.String(250), nullable=False)
+    gender = db.Column (db.String(250), nullable=True)
+    image = db.Column (db.String(250), nullable=False)
+    favoritos = db.relationship("Favorito")
+    @classmethod
+    def create_people(cls, body):
+        try:
+            if body.get("name") is None:
+                raise Exception({
+                    "message": "falta name"
+                }, 400)
+            """if body.get("gender") is None:
+                raise Exception({
+                    "message": "falta gender"
+                }, 400)"""
+            if body.get("image") is None:
+                raise Exception({
+                    "message": "falta image"
+                },400)
+            nuevo_personaje = cls(name=body["name"], gender=body.get("gender"), image=body["image"])
+            guardar_personaje = nuevo_personaje.save_commit()
+            if guardar_personaje is False:
+                raise Exception({
+                    "message": "Error del servidor"
+                },500)
+            return nuevo_personaje
+        except Exception as error:
+            print(error.args, type(error.args))
+            return ({
+                "message": "Algo salio mal (" + error.args[0]["message"] + ")",
+                "status": error.args[1]
+            })
+    def save_commit(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            return False
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            # do not serialize the password, its a security breach
+        }
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column (db.String(250), nullable=False)
+    image = db.Column (db.String(250), nullable=False)
+    favoritos = db.relationship("Favorito")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            # do not serialize the password, its a security breach
+        }
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column (db.String(250), unique=True, nullable=False)
+    password = db.Column (db.String(250), nullable=False)
+    favoritos = db.relationship("Favorito", back_populates="users")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "mail": self.mail,
+            "favoritos": [favorito.serialize() for favorito in self.favoritos]
+            # do not serialize the password, its a security breach
+        }
+    def serialize_two(self):
+        return {
+            "id": self.id,
+            "mail": self.mail
+            #"favoritos": [favorito.serialize() for favorito in self.favoritos]
+            # do not serialize the password, its a security breach
+        }
+class Favorito(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_personaje = db.Column(db.Integer, db.ForeignKey('personaje.id'))
+    id_planeta = db.Column(db.Integer, db.ForeignKey('planeta.id'))
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    users = db.relationship("Usuario", back_populates="favoritos")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_personaje": self.id_personaje,
+            "id_planeta": self.id_planeta,
+            "id_usuario": self.id_usuario,
             # do not serialize the password, its a security breach
         }
