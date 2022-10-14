42f637ac06763a95f761fdaaac7fdd481cc7bac6
gabytochon <gabrielatochon@gmail.com>
2022-10-14 20:21:45 +0000

    Finalizado

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Personaje, Planeta, Usuario
+from models import db, User, Personaje, Planeta, Usuario, Favorito
 #from models import Person
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,10 +18,12 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
@@ -36,6 +38,7 @@ def user_list():
     print(lista_usuario)
     #enviar data
     return jsonify(lista_usuario), 200
+
 @app.route('/users/<int:usuario_id>', methods=['GET'])
 def users_favorites_list(usuario_id=None):
     if usuario_id is not None:
@@ -45,6 +48,7 @@ def users_favorites_list(usuario_id=None):
         return jsonify("Not found"), 404
     else:
         return jsonify("Not found"), 404
+
 @app.route('/people', methods=['GET'])
 def people_list():
     #obtener información de la base de datos
@@ -55,6 +59,7 @@ def people_list():
     print(lista_personajes)
     #enviar data
     return jsonify(lista_personajes), 200
+
 @app.route('/people/<int:people_id>', methods=['GET'])
 def one_people(people_id=None):
     if people_id is not None:
@@ -64,6 +69,7 @@ def one_people(people_id=None):
         return jsonify("Not found"), 404
     else:
         return jsonify("Not found"), 404
+
 @app.route('/planets', methods=['GET'])
 def planets_list():
     #obtener información de la base de datos
@@ -74,6 +80,7 @@ def planets_list():
     print(lista_planeta)
     #enviar data
     return jsonify(lista_planeta), 200
+
 @app.route('/planets/<int:planet_id>', methods=['GET'])
 def one_planet(planet_id=None):
     if planet_id is not None:
@@ -83,6 +90,82 @@ def one_planet(planet_id=None):
         return jsonify("Not found"), 404
     else:
         return jsonify("Not found"), 404
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_planet(planet_id=None):
+    data = request.json
+    if planet_id is not None:
+        planeta = Planeta.query.get(planet_id)
+        if planeta is not None:
+            favorito = Favorito(id_usuario=data["id_usuario"], id_planeta=planet_id)
+            db.session.add(favorito)
+            try:
+                db.session.commit()
+                return jsonify(favorito.serialize()), 201
+            except Exception as error:
+                print(error.args)
+                db.session.rollback()
+                return jsonify("Not found"), 500
+        else:
+            return jsonify("Not found"), 404
+    else:
+        return jsonify("Not found"), 404
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_persona(people_id=None):
+    data = request.json
+    if people_id is not None:
+        persona = Personaje.query.get(people_id)
+        if persona is not None:
+            favorito = Favorito(id_usuario=data["id_usuario"], id_personaje=people_id)
+            db.session.add(favorito)
+            try:
+                db.session.commit()
+                return jsonify(favorito.serialize()), 201
+            except Exception as error:
+                print(error.args)
+                db.session.rollback()
+                return jsonify("Not found"), 500
+        else:
+            return jsonify("Not found"), 404
+    else:
+        return jsonify("Not found"), 404
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id=None):
+    if planet_id is not None:
+        favorito = Favorito.query.filter_by(id_planeta=planet_id).first()
+        if favorito is not None:
+            try:
+                db.session.delete(favorito)
+                db.session.commit()
+                return jsonify ([]), 204
+            except Exception as error:
+                db.session.rollback()
+                return jsonify("error intente mas tarde"), 500
+        else:
+            return jsonify("Not found"), 404
+    else:
+        return jsonify("Bad request"), 400
+        
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_persona(people_id=None):
+    if people_id is not None:
+        favorito = Favorito.query.filter_by(id_personaje=people_id).first()
+        if favorito is not None:
+            try:
+                db.session.delete(favorito)
+                db.session.commit()
+                return jsonify ([]), 204
+            except Exception as error:
+                db.session.rollback()
+                return jsonify("error intente mas tarde"), 500
+        else:
+            return jsonify("Not found"), 404
+    else:
+        return jsonify("Bad request"), 400
+
+
 @app.route('/people', methods=['POST'])
 def post_people():
     body = request.json
